cd19ed6e4f95979fa93f0768b99df0cbc9614c5e
Dmitry Saprykin <saprykin.dmitry.vitalyevich@gmail.com>
2018-10-12 08:08:38 +0300

    Refactoring code

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import { fromEvent } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 const inputSearch     = document.getElementById('search') as HTMLInputElement;
+const divContent      = document.getElementById('result') as HTMLDivElement;
 const changeSequence$ = fromEvent(document, 'keyup');
 
 function request() {
     return fetch('https://api.github.com/search/repositories?q=' + inputSearch.value)
-        .then((res) => res.json());
+        .then((res) => {
+            if (res.status === 200) {
+                return res.json();
+            }
+            return null;
+        });
 }
 
 const requestSequence$ = changeSequence$.pipe(
@@ -15,9 +21,26 @@ const requestSequence$ = changeSequence$.pipe(
     )
 );
 
+type r = {
+    html_url: string
+};
+
 setTimeout(() => {
     requestSequence$.subscribe((res) => {
         // tslint:disable-next-line
         console.log(res);
+        if (null !== res) {
+            const data = res.items;
+            // tslint:disable-next-line
+            console.log(data);
+            divContent.innerHTML = '';
+            data.forEach((repo: r) => {
+                const p = document.createElement('p');
+                p.textContent = `${repo.html_url}`;
+                divContent.appendChild(p);
+            });
+        } else {
+            divContent.innerHTML = '';
+        }
     });
-}, 1000);
+}, 3000);
